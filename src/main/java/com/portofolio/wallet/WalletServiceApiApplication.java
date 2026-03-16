c9069aea6e53b14d70a421dc5ddcc08516776a7a
Riski <reesky.it@gmail.com>
2026-03-16 11:27:06 +0700

    initial project setup

---
+++ src/main/java/com/portofolio/wallet/WalletServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.portofolio.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portofolio/wallet/WalletServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portofolio.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
