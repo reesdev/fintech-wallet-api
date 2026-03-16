23cb1bdfc128da0ba0a864e1657389419626a927
Riski <reesky.it@gmail.com>
2026-03-16 18:39:00 +0700

    feat: implement JwtAuthenticationFilter for JWT authentication

--- src/main/java/com/portofolio/wallet/config/SecurityConfig.java
+++ src/main/java/com/portofolio/wallet/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package com.portofolio.wallet.config;
 
+import com.portofolio.wallet.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtFilter) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .anyRequest().authenticated());
-    return http.build();
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
